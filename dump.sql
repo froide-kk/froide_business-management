a86fb8a020c79cbe9399029e2efec91b2ecdd491
froide-tamamiyamaue <tamami-yamaue@froide.co.jp>
2022-05-31 13:34:58 +0900

    challenge２

--- dump.sql
+++ dump.sql
@@ -1,2 +1,8 @@
 Enter password: 
 mysqldump: Got error: 1045: Access denied for user 'root'@'localhost' (using password: YES) when trying to connect
+
+CREATE TABLE name_age_list(
+    id INT(11) AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL ,
+    age INT(3) NOT NULL,
+    PRIMARY KEY (id));
