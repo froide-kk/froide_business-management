fd39e1c800d0b3a6568dfed12b17b956a524a987
froide-t.nakashima <takeru-nakashima@froide.co.jp>
2022-06-02 16:35:09 +0900

    キャッシュ削除

--- HelloWorld/src/main/java/work_histories.java
+++
@@ -1,2 +0,0 @@
-public class work_histories {
-}
--- Sample/menu/main.js
+++
@@ -1,5 +0,0 @@
-import './style.css'
-
-document.querySelector(".open-menu-button").addEventListener("click", () => { // メニューのボタンをクリックしたら
-  document.querySelector(".menu-container").classList.toggle("is-open") // メニュー本体のcssのクラスをつけ外しし、表示・非表示を切り替える
-})
