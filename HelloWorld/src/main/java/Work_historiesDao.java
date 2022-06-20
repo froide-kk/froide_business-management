d1b655a10ba6a0a43e4375f7989182770586549f
froide- m-izumi <motoya-izumi@froide.co.jp>
2022-06-20 10:04:51 +0900

    Work_histriesDao上げ忘れ

--- HelloWorld/src/main/java/Work_historiesDao.java
+++ HelloWorld/src/main/java/Work_historiesDao.java
@@ -12,6 +12,20 @@ public interface Work_historiesDao {
     @Delete
     int delete(Work_histories work_histories);
 
-//    @Select
-//    Work_histories selectById(Integer id);
+    @Select
+    Work_histories selectById(Integer id);
+
+    @Select
+    List<Work_histories> selectByAll(Integer id);
+
+    @Update(include = {"id","industry","system_sum","system_details","role","dev_environment"})
+    int updateWorkHistory(Work_histories work_histories);
+    @Update(include = {"id","project_id"})
+    int updateProjectID(Work_histories work_histories);
+    @Update(include = {"id","dev_scale"})
+    int updateDevScale(Work_histories work_histories);
+    @Update(include = {"id","work_start","work_end"})
+    int updateWorkStartEnd(Work_histories work_histories);
+
+
 }
